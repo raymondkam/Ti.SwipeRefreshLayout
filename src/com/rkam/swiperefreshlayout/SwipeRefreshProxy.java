db81d8c277dc41c073fee97a72028676775eb7fb
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-02-12 14:13:14 +0100

    Fix threading issues, update to SDK 6.0.0+

--- src/com/rkam/swiperefreshlayout/SwipeRefreshProxy.java
+++ src/com/rkam/swiperefreshlayout/SwipeRefreshProxy.java
@@ -1,16 +1,22 @@
 package com.rkam.swiperefreshlayout;
 
 import org.appcelerator.kroll.annotations.Kroll;
+import org.appcelerator.kroll.KrollProxy;
 import org.appcelerator.titanium.proxy.TiViewProxy;
 import org.appcelerator.titanium.view.TiUIView;
+import org.appcelerator.titanium.TiApplication;
 
 import android.app.Activity;
+import android.os.Handler;
+import android.os.Message;
 
 @Kroll.proxy(creatableInModule=SwiperefreshlayoutModule.class)
-public class SwipeRefreshProxy extends TiViewProxy {
+public class SwipeRefreshProxy extends TiViewProxy implements Handler.Callback {
 
 	private SwipeRefresh swipeRefresh;
-	
+
+	protected static final int MSG_SET_REFRESHING = KrollProxy.MSG_LAST_ID + 101;
+
 	public SwipeRefreshProxy() {
 		super();
 	}
@@ -20,15 +26,39 @@ public class SwipeRefreshProxy extends TiViewProxy {
 		swipeRefresh = new SwipeRefresh(this);
 		return this.swipeRefresh;
 	}
+    
+	/* Public API */
 	
 	@Kroll.method
 	public void setRefreshing(boolean refreshing) {
-		this.swipeRefresh.setRefreshing(refreshing);
-		return;
+        if (TiApplication.isUIThread()) {
+            doSetRefreshing(refreshing);
+        } else {
+            Message message = getMainHandler().obtainMessage(MSG_SET_REFRESHING, refreshing);
+            message.sendToTarget();
+        }
+
 	}
-	
+    
 	@Kroll.method @Kroll.getProperty
 	public boolean isRefreshing() {
 		return this.swipeRefresh.isRefreshing();
+	}
+    
+	/* Utilities */
+    
+	public boolean handleMessage(Message message) {
+		switch (message.what) {
+			case MSG_SET_REFRESHING: {
+				doSetRefreshing((Boolean) message.obj);
+				return true;
+			}
+		}
+		
+		return super.handleMessage(message);
+	}
+	
+	protected void doSetRefreshing(boolean refreshing) {
+		this.swipeRefresh.setRefreshing(refreshing);
 	}
 }
